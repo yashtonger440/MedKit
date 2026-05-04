68643417cdc33e02f9df0b16ea6a5edb0317b92c
Yash <yash123@gmail.com>
2026-05-04 11:29:47 +0530

    bug fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -9,7 +9,7 @@ API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    req.headers.Authorization = token;
+    req.headers.Authorization =  `Bearer ${token}`;
   }
 
   return req;
