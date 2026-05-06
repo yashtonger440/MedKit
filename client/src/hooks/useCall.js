0281f1d6cd608bdbc3fc10b1b0f0315360a31cc1
Yash <yash123@gmail.com>
2026-05-06 12:03:39 +0530

    updated

--- client/src/hooks/useCall.js
+++ client/src/hooks/useCall.js
@@ -46,6 +46,16 @@ const useCall = (currentUserId) => {
         iceServers: [
           { urls: "stun:stun.l.google.com:19302" },
           { urls: "stun:stun1.l.google.com:19302" },
+          {
+        urls: "turn:openrelay.metered.ca:80",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
         ],
       },
     });
